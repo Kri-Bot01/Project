84ee46653a2d54ab2e698132a13f4bf35a1b6a21
Kri-Bot01 <krishivjain8a@gmail.com>
2024-05-28 23:09:15 +0530

    url linking

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,aboutcompany
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="homepage"),
+    path('aboutus',aboutcompany,name="aboutpage")
 ]
